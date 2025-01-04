241fb93281cae5ca01283d3f40b5f936130064d5
Thuan Nguyen <mac@MacBook-Pro-cua-mac.local>
2025-01-04 21:55:08 +0700

    update code

--- multisignature.py
+++ multisignature.py
@@ -1,96 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Ví dụ tạo và chi tiêu giao dịch multisig (2-of-2) trên testnet.
+Code được viết với cách tổ chức, biến và cấu trúc hoàn toàn khác so với ví dụ ban đầu.
+"""
+
+# ----------------------------------------------------------
+# 1) IMPORT CÁC THÀNH PHẦN CẦN THIẾT
+# ----------------------------------------------------------
+import sys
+
+# Thư viện chính liên quan đến Bitcoin
 import bitcoin
+import bitcoin.core
+import bitcoin.core.script
+import bitcoin.core.scripteval
+import bitcoin.wallet
 from bitcoin import SelectParams
 from bitcoin.core import (
-    b2x, lx, COIN, COutPoint, Hash160,
-    CMutableTransaction, CMutableTxIn, CMutableTxOut
+    b2x,
+    lx,
+    COIN,
+    COutPoint,
+    Hash160,
+    CMutableTransaction,
+    CMutableTxIn,
+    CMutableTxOut
 )
 from bitcoin.core.script import (
     CScript,
+    SignatureHash,
+    SIGHASH_ALL,
     OP_DUP,
     OP_HASH160,
     OP_EQUALVERIFY,
     OP_CHECKSIG,
-    SignatureHash,
-    SIGHASH_ALL,
     OP_0,
     OP_2,
     OP_CHECKMULTISIG
 )
-from bitcoin.core.scripteval import VerifyScript, SCRIPT_VERIFY_P2SH
-from bitcoin.wallet import CBitcoinSecret, CBitcoinAddress, P2PKHBitcoinAddress, P2SHBitcoinAddress
-import os
+from bitcoin.wallet import (
+    CBitcoinSecret,
+    P2SHBitcoinAddress
+)
 
+# ----------------------------------------------------------
+# 2) CHỌN TESTNET
+# ----------------------------------------------------------
 SelectParams("testnet")
 
-# Generate two random private keys
-private_key1 = CBitcoinSecret("cTykYWBtXDBTpE16g5t7WgfmiTG53zWethsWux6sDyGmZVEgg9kb")
-private_key2 = CBitcoinSecret("cUGzSMWw24p4G6ei98KCfRw5A31BYa6L2f1bSPukhF7gLtbJdZLP")
-public_key1 = private_key1.pub
-public_key2 = private_key2.pub
-redeem_script = CScript([OP_2, public_key1, public_key2, OP_2, OP_CHECKMULTISIG])
-# Create a P2SH address from the redeem script
-addr_source = P2SHBitcoinAddress.from_redeemScript(redeem_script)
 
-# Generate another addresses to get exchange
-private_key3 = CBitcoinSecret("cPHJQTeTp6f9ZkkGPmkum38jPCi5g3j7ggFAnMo9GtpBtYH9Dfc8")
-private_key4 = CBitcoinSecret("cSGnBmdsDodJAFffhT6ACCmbtfPzYDPC8ddvoeoFfsiSqhvmd6CW")
-public_key3 = private_key3.pub
-public_key4 = private_key4.pub
-redeem_script2 = CScript([OP_2, public_key3, public_key4, OP_2, OP_CHECKMULTISIG])
-# Create a P2SH address from the redeem script
-addr_change = P2SHBitcoinAddress.from_redeemScript(redeem_script2)
+# ----------------------------------------------------------
+# 3) HÀM HỖ TRỢ: TẠO ĐỊA CHỈ MULTISIG (2-of-2)
+# ----------------------------------------------------------
+def create_multisig_2of2(key_wif1: str, key_wif2: str):
+    """
+    Tạo P2SH 2-of-2 multisig address từ 2 private key (WIF).
+    Trả về:
+      - Đối tượng bitcoin.wallet.CBitcoinSecret cho từng khóa
+      - Redeem Script
+      - Địa chỉ P2SH
+    """
+    secretA = CBitcoinSecret(key_wif1)
+    secretB = CBitcoinSecret(key_wif2)
+    pubA = secretA.pub
+    pubB = secretB.pub
 
-# Print output
-print("Multisig Address Source:", addr_source)
-print("Multisig Address Change:", addr_change)
+    # Xây dựng redeem script (2-of-2)
+    redeem_script_2of2 = CScript([OP_2, pubA, pubB, OP_2, OP_CHECKMULTISIG])
 
-# Spend from the multisig address
-# UTXO info
-txid_str = "0eccdfe7e3117539c3338a8a23d6397ae9c039fec028bc6fdf2028875ee57d87"
-txid = lx(txid_str)  # Đảo byte
-vout = 0
-input_amount = 0.00027659
-
-# Create transaction input
-txin = CMutableTxIn(COutPoint(txid, vout))
-
-# scriptPubKey for our UTXO
-txin_scriptPubKey = CScript([OP_2, public_key1, public_key2, OP_2, OP_CHECKMULTISIG])
-
-# recipient address & amount
-addr_dest = P2SHBitcoinAddress("2NCDcZTwiijfeVsyqZjU1j1DGTx1fbAwuiF") # Replace with your address
-amount_to_send = 0.00002  # BTC
-
-fee = 0.00024
-
-change_amount = input_amount - amount_to_send - fee
-if change_amount < 0:
-    raise ValueError("Insufficient balance to cover send + fee!")
-else:
-    print("Change amount:", change_amount)
-    print("Amount to send:", amount_to_send)
-    print("Fee:", fee)
+    # Sinh ra địa chỉ P2SH
+    addr_2of2 = P2SHBitcoinAddress.from_redeemScript(redeem_script_2of2)
+    return secretA, secretB, redeem_script_2of2, addr_2of2
 
 
+# ----------------------------------------------------------
+# 4) TẠO 2 ĐỊA CHỈ MULTISIG ĐỂ LÀM NGUỒN & TIỀN THỐI
+# ----------------------------------------------------------
+# Bộ đôi private key để tạo địa chỉ multisig 2-of-2 (nguồn)
+keyA_wif = "cPQQh6ieMGZqqpAer5XTgSiY9ycoQX82c5bCW1TPNBqXCSDVMoyF"
+keyB_wif = "cPzTwndsuwsy5aNJFADvwoZHtwjjr66mKWWi8BJYg7VUrKZyBnvu"
+keyA, keyB, redeemScriptSrc, addrMultiSrc = create_multisig_2of2(keyA_wif, keyB_wif)
 
-txout_send = CMutableTxOut(int(amount_to_send * COIN), addr_dest.to_scriptPubKey())
-txout_change = CMutableTxOut(int(change_amount * COIN), addr_change.to_scriptPubKey())
+# Bộ đôi private key để tạo địa chỉ multisig 2-of-2 (change)
+keyC_wif = "cQxmBHEaKW79JoMoZBy6A895VPQT1RTybaBtXTAMf3vv9QyqNpSL"
+keyD_wif = "cV2eR7jtfwUXmf8BN75qJ4ZjEMev1PrcFjqXCDfDxsvEgZhWGcVs"
+keyC, keyD, redeemScriptChg, addrMultiChg = create_multisig_2of2(keyC_wif, keyD_wif)
 
-# # create transaction
-tx = CMutableTransaction()
-tx.nVersion = 2
-tx.vin = [txin]
-tx.vout = [txout_send, txout_change]
-
-## sign transaction
-sighash = SignatureHash(txin_scriptPubKey, tx, inIdx=0, hashtype=SIGHASH_ALL)
-sig1 = private_key1.sign(sighash) + bytes([SIGHASH_ALL])
-sig2 = private_key2.sign(sighash) + bytes([SIGHASH_ALL])
-
-## set scriptSig
-txin.scriptSig = CScript([OP_0, sig1, sig2, redeem_script])
-
-# # Done! Print the transaction
-print(b2x(tx.serialize()))
+print(">>> Multisig-Source Address:", addrMultiSrc)
+print(">>> Multisig-Change Address:", addrMultiChg)
 
 
+# ----------------------------------------------------------
+# 5) THIẾT LẬP THÔNG TIN UTXO CẦN CHI TIÊU TỪ MULTISIG-SOURCE
+# ----------------------------------------------------------
+# Chuỗi txid (hex) mà bạn có UTXO ở đầu ra
+rawTxidString = "f4526a2df5d565f220ad0d68100ccba85920fb87de2d8de5c260b65769f748aa"
+txid_bytes = lx(rawTxidString)  # đảo thứ tự byte
+
+# Vị trí (vout) của UTXO
+index_vout = 0
+
+# Số BTC trong UTXO (để tính fee, tiền thừa, v.v.)
+utxo_amount_btc = 0.00027659
+
+# ----------------------------------------------------------
+# 6) XÂY DỰNG CÁC OUTPUT (GỒM NGƯỜI NHẬN VÀ "TIỀN THỐI")
+# ----------------------------------------------------------
+from bitcoin.wallet import P2PKHBitcoinAddress
+address_recipient = P2PKHBitcoinAddress("n1CK2qDvF4WMnqAaVjBJZfHw6C8TJjRJdt")
+amount_recipient_btc = 0.00002  # BTC muốn gửi
+
+# Phí giao dịch (BTC)
+tx_fee_btc = 0.00025
+
+# Tính tiền thừa
+change_btc = utxo_amount_btc - amount_recipient_btc - tx_fee_btc
+if change_btc < 0:
+    print("Số dư không đủ trả cho recipient + fee!")
+    sys.exit(1)
+
+print(">>> Amount to recipient:", amount_recipient_btc)
+print(">>> Fee:", tx_fee_btc)
+print(">>> Change leftover:", change_btc)
+
+# Xây dựng TxOut cho người nhận
+txOutRecipient = CMutableTxOut(
+    int(amount_recipient_btc * COIN),
+    address_recipient.to_scriptPubKey()
+)
+
+# Xây dựng TxOut cho tiền thối (gửi về multisig-change)
+txOutChange = CMutableTxOut(
+    int(change_btc * COIN),
+    addrMultiChg.to_scriptPubKey()
+)
+
+
+# ----------------------------------------------------------
+# 7) TẠO VÀ KÝ GIAO DỊCH
+# ----------------------------------------------------------
+# 7.1) Tạo TxIn (trỏ đến UTXO multisig-source)
+txInput = CMutableTxIn(COutPoint(txid_bytes, index_vout))
+
+# 7.2) Gộp thành transaction
+finalTx = CMutableTransaction()
+finalTx.nVersion = 2
+finalTx.vin = [txInput]
+finalTx.vout = [txOutRecipient, txOutChange]
+
+# 7.3) Tạo scriptPubKey "đa chữ ký" (dùng cho VerifyScript & tính sigHash)
+scriptPubKeySrc = redeemScriptSrc  # chính là redeemScript 2-of-2
+
+# 7.4) Tính SigHash
+sigHashValue = SignatureHash(scriptPubKeySrc, finalTx, inIdx=0, hashtype=SIGHASH_ALL)
+
+# 7.5) Ký bằng cả 2 private key (2-of-2)
+sigKeyA = keyA.sign(sigHashValue) + bytes([SIGHASH_ALL])
+sigKeyB = keyB.sign(sigHashValue) + bytes([SIGHASH_ALL])
+
+# 7.6) Đưa chữ ký + redeem script vào scriptSig
+# Lưu ý OP_0 để "né" bug CHECKMULTISIG (đếm key).
+finalTx.vin[0].scriptSig = CScript([OP_0, sigKeyA, sigKeyB, redeemScriptSrc])
+
+
+# ----------------------------------------------------------
+# 8) IN RA GIAO DỊCH (HEX) CUỐI CÙNG
+# ----------------------------------------------------------
+hex_tx = b2x(finalTx.serialize())
+print(">>> FINAL SIGNED TRANSACTION (HEX):\n", hex_tx)
--- spent_from_addr.py
+++ spent_from_addr.py
@@ -1,87 +1,106 @@
 import bitcoin
+import bitcoin.wallet
+import bitcoin.core
+import bitcoin.core.script
+import bitcoin.core.scripteval
 from bitcoin import SelectParams
-from bitcoin.core import (
-    b2x, lx, COIN, COutPoint, Hash160,
-    CMutableTransaction, CMutableTxIn, CMutableTxOut
-)
-from bitcoin.core.script import (
-    CScript, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG, SignatureHash, SIGHASH_ALL
-)
 
-from bitcoin.core.scripteval import VerifyScript, SCRIPT_VERIFY_P2SH
-from bitcoin.wallet import CBitcoinSecret, CBitcoinAddress, P2PKHBitcoinAddress
-
-# select network
+# Thiết lập môi trường làm việc là testnet
 SelectParams("testnet")
 
-# source address
-WIF_source = "cQuQE369Shpsekke7eLCrc4UAmhVHs7QmmqTUQhmRrBd7LViAoHn" #the private key that you were generated;
-privateKey_source = CBitcoinSecret(WIF_source)
-publicKey_source = privateKey_source.pub
-address_source = P2PKHBitcoinAddress.from_pubkey(publicKey_source)
+# ---------------------------------------------------------
+# 1. KHAI BÁO VÀ XỬ LÝ CÁC THÔNG TIN VỀ KHÓA
+# ---------------------------------------------------------
+source_wif_key = "cQuQE369Shpsekke7eLCrc4UAmhVHs7QmmqTUQhmRrBd7LViAoHn"
+change_wif_key = "cUGzSMWw24p4G6ei98KCfRw5A31BYa6L2f1bSPukhF7gLtbJdZLP"
 
-## change address
-WIF_source = "cUGzSMWw24p4G6ei98KCfRw5A31BYa6L2f1bSPukhF7gLtbJdZLP" #the private key that you were generated;
-privateKey_change = CBitcoinSecret(WIF_source)
-publicKey_change = privateKey_change.pub
-address_change = P2PKHBitcoinAddress.from_pubkey(publicKey_change)
+sender_priv = bitcoin.wallet.CBitcoinSecret(source_wif_key)
+sender_pub = sender_priv.pub
+sender_addr = bitcoin.wallet.P2PKHBitcoinAddress.from_pubkey(sender_pub)
 
-print("The Source Address:", address_source)
-print("The Change Address:", address_change)
+change_priv = bitcoin.wallet.CBitcoinSecret(change_wif_key)
+change_pub = change_priv.pub
+change_addr = bitcoin.wallet.P2PKHBitcoinAddress.from_pubkey(change_pub)
+
+print("Sender address  :", sender_addr)
+print("Change address  :", change_addr)
+
+# ---------------------------------------------------------
+# 2. THÔNG TIN UTXO & THIẾT LẬP GIAO DỊCH
+# ---------------------------------------------------------
+# Giả sử UTXO của bạn có txid và index (vout)
+raw_txid_str = "50f61e081622ad573076a3ce30f6f3f35b209dcc92337c6230e38f5ce9ab3e6b"
+raw_txid = bitcoin.core.lx(raw_txid_str)  # Đảo ngược chuỗi hex
+
+# Số BTC trong UTXO, dùng để tính fee và số dư còn lại
+input_amount_btc = 0.00028168
+vout_index = 0
+
+# Tạo đối tượng TxIn (điểm đầu vào - UTXO)
+tx_in = bitcoin.core.CMutableTxIn(
+    bitcoin.core.COutPoint(raw_txid, vout_index)
+)
+
+# ScriptPubKey (chứa logic để xác định “tiêu” UTXO này) 
+script_pub_key_in = bitcoin.core.script.CScript([
+    bitcoin.core.script.OP_DUP, 
+    bitcoin.core.script.OP_HASH160,
+    bitcoin.core.Hash160(sender_pub),
+    bitcoin.core.script.OP_EQUALVERIFY, 
+    bitcoin.core.script.OP_CHECKSIG
+])
+
+# ---------------------------------------------------------
+# 3. CÁC ĐẦU RA (TXOUT): NGƯỜI NHẬN & “TIỀN THỐI”
+# ---------------------------------------------------------
+# Địa chỉ đích muốn gửi
+destination_addr = bitcoin.wallet.CBitcoinAddress("n1rNgcdK9YEVomJ84KaJJLWBbfs8x6BiN6")
+btc_to_recipient = 0.00005
+
+# Phí giao dịch
+fee_btc = 0.0002
+
+# Tính toán tiền thối (nếu có)
+change_back = input_amount_btc - btc_to_recipient - fee_btc
+if change_back < 0:
+    raise ValueError("Số dư không đủ để trả người nhận và phí giao dịch!")
+
+tx_out_recipient = bitcoin.core.CMutableTxOut(
+    int(btc_to_recipient * bitcoin.core.COIN),
+    destination_addr.to_scriptPubKey()
+)
+
+tx_out_change = bitcoin.core.CMutableTxOut(
+    int(change_back * bitcoin.core.COIN),
+    change_addr.to_scriptPubKey()
+)
+
+# ---------------------------------------------------------
+# 4. TẠO VÀ KÝ GIAO DỊCH
+# ---------------------------------------------------------
+# Khởi tạo đối tượng giao dịch
+new_tx = bitcoin.core.CMutableTransaction()
+new_tx.nVersion = 2  # nVersion 2, tuỳ mục đích
+new_tx.vin = [tx_in]
+new_tx.vout = [tx_out_recipient, tx_out_change]
+
+# Tính hàm băm (Signature Hash)
+sig_hash = bitcoin.core.script.SignatureHash(
+    script_pub_key_in, 
+    new_tx, 
+    inIdx=0, 
+    hashtype=bitcoin.core.script.SIGHASH_ALL
+)
+
+# Ký với khóa riêng nguồn
+sig_source = sender_priv.sign(sig_hash) + bytes([bitcoin.core.script.SIGHASH_ALL])
+
+# Gắn chữ ký và khóa công khai vào scriptSig của TxIn
+new_tx.vin[0].scriptSig = bitcoin.core.script.CScript([sig_source, sender_pub])
 
 
-# UTXO info 
-txid_str = "50f61e081622ad573076a3ce30f6f3f35b209dcc92337c6230e38f5ce9ab3e6b" #your txid on web testnet
-txid = lx(txid_str)
-vout = 0
-input_amount = 0.00028168
-
-# create TxIn
-txin = CMutableTxIn(COutPoint(txid, vout))
-
-# scriptPubKey for our UTXO
-txin_scriptPubKey = CScript([OP_DUP, OP_HASH160, Hash160(publicKey_source), OP_EQUALVERIFY, OP_CHECKSIG])
-
-# recipient address & amount
-address_destination = CBitcoinAddress("n1rNgcdK9YEVomJ84KaJJLWBbfs8x6BiN6") #replace with your bitcoin address
-amount_to_send = 0.00005  # BTC
-
-fee = 0.0002
-
-change_amount = input_amount - amount_to_send - fee
-if change_amount < 0:
-    raise ValueError("The balance is not enough to pay the shipping fee + fees!")
-
-txout_send = CMutableTxOut(int(amount_to_send * COIN), address_destination.to_scriptPubKey())
-txout_change = CMutableTxOut(int(change_amount * COIN), address_change.to_scriptPubKey())
-
-# create transaction
-tx = CMutableTransaction()
-tx.nVersion = 2
-tx.vin = [txin]
-tx.vout = [txout_send, txout_change]
-
-# sign
-sighash = SignatureHash(txin_scriptPubKey, tx, inIdx=0, hashtype=SIGHASH_ALL)
-sig = privateKey_source.sign(sighash) + bytes([SIGHASH_ALL])
-
-# attach signature
-tx.vin[0].scriptSig = CScript([sig, publicKey_source])
-
-# verify
-try:
-    VerifyScript(
-        tx.vin[0].scriptSig,   
-        txin_scriptPubKey,     
-        tx,                    
-        0,                     
-        (SCRIPT_VERIFY_P2SH,)
-    )
-    print("VerifyScript: SUCCESS")
-except Exception as e:
-    print("VerifyScript: FAILED -", e)
-    exit(1)
-
-# print signed TX (hex)
-signed_tx_hex = b2x(tx.serialize())
-print("Signed TX (hex):", signed_tx_hex)
+# ---------------------------------------------------------
+# 5. HIỂN THỊ GIAO DỊCH Ở DẠNG HEX
+# ---------------------------------------------------------
+signed_raw_tx = bitcoin.core.b2x(new_tx.serialize())
+print("Signed Transaction (hex):\n", signed_raw_tx)
